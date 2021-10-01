38e744c4b794fa2724dee26ff0462d3b20f8ba93
zamanmughal98 <57499526+zamanmughal98@users.noreply.github.com>
2021-09-30 21:44:26 -0700

    3 Different Functions to Detect Duplicate value
    3 Different Functions to detect the Duplicate value in any Array.
    
    First function works with 2 pointers( i , j ), checks if the value of both indexes is the same or not.
    
    Second function works with 2 different arrays other than one that will be given to the function as an argument. Take the value one after another from the original array and push it in one array only when the same value isn't available in that array. And if the value is already present there, then it should maintain the duplicate array and pass the value in it too. The fun part is, the function will also make sure that there isn't duplicate value inside the duplicate array. ( consider the case where you have the same number more than 2 times)
    
    Third Function's functionality is based on the second function but the difference is this time function is not iterative but Recursive, and the rest of the functionality is the same.

--- DetectDuplicateFunction.js
+++ DetectDuplicateFunction.js
@@ -1,18 +1,11 @@
-let arr = [];
+arr = [];
+check_Array=[];
+duplicate_array=[]
 var array_len = Number(prompt("How many values you want to enter in Array ??", "5"));
 for (i = 0; i < array_len; i++) {
     arr.push(Number(prompt("Enter a value please !")));
 }
 console.log("Printing Whole Array " + arr);
-function detectDuplicate1(array1) {
-    for (var i = 0; i < array1.length - 1; i++) {
-        for (var j = i + 1; j < array1.length; j++) {
-            if (array1[i] === array1[j]) {
-                console.log("Duplicate value (function# 1)==> " + array1[i]);
-            }
-        }
-    }
-}
 function isIncludes(array1, num) {
     for (var i = 0; i < array1.length; i++) {
         if (num === array1[i]) {
@@ -21,7 +14,16 @@ function isIncludes(array1, num) {
     }
     return false;
 }
-function detectDuplicate2(array1) {
+function detectDuplicate_Iterative1(array1) {
+    for (var i = 0; i < array1.length - 1; i++) {
+        for (var j = i + 1; j < array1.length; j++) {
+            if (array1[i] === array1[j]) {
+                console.log("Duplicate value (function# 1)==> " + array1[i]);
+            }
+        }
+    }
+}
+function detectDuplicate_Iterative2(array1) {
     checkArray = [];
     dupArray = [];
     for (var i = 0; i < array1.length; i++) {
@@ -35,5 +37,26 @@ function detectDuplicate2(array1) {
     checkArray.length = 0;        //delecting the array from memory
     return dupArray;
 }
-detectDuplicate1(arr); //Function Calling and passing the array as argument;
-console.log("Duplicate Values (Function# 2)==> " + detectDuplicate2(arr)); //Function Calling and passing the array as argument
+function detectDuplicate_Recursive(start){
+    if(start === array_len){
+        return 0; 
+    }
+    else if (!isIncludes(check_Array, arr[start])) {
+        check_Array.push(Number(arr[start]));
+        start++;
+        detectDuplicate_Recursive(start);
+    }
+    else if (!isIncludes(duplicate_array, arr[start])){
+        duplicate_array.push(Number(arr[start]));
+        start++;
+        detectDuplicate_Recursive(start);
+    }
+    else{
+        start++;
+        detectDuplicate_Recursive(start);
+    }   
+}
+//Functions Calls 
+detectDuplicate_Iterative1(arr);
+console.log("Duplicate Values (Function# 2)==> " + detectDuplicate_Iterative2(arr));
+detectDuplicate_Recursive(0);console.log("Duplicate Values (Function# 3)==> " + duplicate_array);
